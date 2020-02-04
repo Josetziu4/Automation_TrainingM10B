c70d0046ad5574dd7fee3c25417dd95e75bda86b
Aperez11 <alejandro.perez@4thsource.com>
2020-02-04 16:15:10 -0600

    This is my new branch

---
+++ Automation_TrainingM10B/Test Data/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Automation_TrainingM10B.Test_Data
+{
+    class Data
+    {
+        public static IEnumerable sumData
+        {
+            get
+            {
+                yield return new NUnit.Framework.TestCaseData(2, 3).Returns(5);
+                yield return new NUnit.Framework.TestCaseData(3, 3).Returns(6);
+                yield return new NUnit.Framework.TestCaseData(8, 4).Returns(12);
+                yield return new NUnit.Framework.TestCaseData(7, 7).Explicit();
+                //Instead of Returns in can be used Explicit, it will only display the result
+            }
+        }
+    }
+}

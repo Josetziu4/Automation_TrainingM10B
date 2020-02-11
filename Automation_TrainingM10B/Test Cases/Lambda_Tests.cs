53db3b774c9e09147515a486973927335bf2234b
Josetziu4 <47648101+Josetziu4@users.noreply.github.com>
2020-02-11 15:45:50 -0600

    Added Lambda Test Data

---
+++ Automation_TrainingM10B/Test Cases/Lambda_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_TrainingM10B.Test_Cases
+{
+    class Lambda_Tests
+    {
+        List<int> numbers = new List<int>() { 1, 7, 4, 2, 5, 3, 9, 8, 6 };
+
+        public static List<int> FindEvenNumbers(List<int> numbers)
+        {
+            List<int> onlyEvens = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                    onlyEvens.Add(number);
+            }
+            return onlyEvens;
+        }
+
+        public static bool IsEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+    }
+}

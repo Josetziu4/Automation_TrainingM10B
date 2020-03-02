50a8e5025d5ba631331ea5b9657711aecce40aad
Josetziu4 <47648101+Josetziu4@users.noreply.github.com>
2020-03-02 17:27:33 -0600

    3/2/2020 Module 11: API Rest

--- Automation_TrainingM10B/Test Cases/API_Tests.cs
+++ Automation_TrainingM10B/Test Cases/API_Tests.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using NUnit.Framework;
 
 namespace Automation_TrainingM10B.Test_Cases
 {
     class API_Tests
     {
+        HttpWebRequest HttpRequest;
+        HttpWebResponse HttpResponse;
+
+        Stream DataStream;
+        StreamReader DataReader;
+        StreamWriter DataWriter;
+
+        string Payload;
+
+        [Test]
+        public void GetTest()
+        {
+            HttpRequest = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/employees");
+            HttpRequest.Method = "GET";
+            HttpRequest.ContentType = "application/json";
+            HttpRequest.KeepAlive = false;
+
+            HttpResponse = (HttpWebResponse)HttpRequest.GetResponse();
+
+            using(DataStream = HttpResponse.GetResponseStream())
+            {
+                DataReader = new StreamReader(DataStream);
+                Payload = DataReader.ReadToEnd();
+            }
+            HttpResponse.Close();
+
+            Console.WriteLine(Payload);
+        }
     }
 }

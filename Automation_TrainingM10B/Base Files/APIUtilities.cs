9a0386f3dd80a617db3ecfa07044e649c52a5889
Hugo Vidal <hugo.vidal@agilethought.com>
2020-03-05 16:36:54 -0600

    Adding the methods to APIUtilities

---
+++ Automation_TrainingM10B/Base Files/APIUtilities.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_TrainingM10B.Base_Files
+{
+    class APIUtilities
+    {
+        //VARIABLE
+        HttpWebRequest HttpRequest;
+        HttpWebResponse HttpResponse;
+        Stream DataStream;
+        StreamReader DataReader;
+        StreamWriter DataWriter;
+        string Payload;
+
+        [OneTimeSetUp]
+        public void GetTest()
+        {
+
+        }
+
+        [SetUp]
+        public void BeforeTest()
+        {
+
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+
+        }
+
+        public void fnGetMethod(string pstrAPIUrl)
+        {          
+            HttpRequest = (HttpWebRequest)WebRequest.Create(pstrAPIUrl); // we send the URL of the GET API
+            HttpRequest.Method = "GET";
+            HttpRequest.ContentType = "application/json";
+            HttpRequest.KeepAlive = false;
+
+            HttpResponse = (HttpWebResponse)HttpRequest.GetResponse();
+
+            using (DataStream = HttpResponse.GetResponseStream())
+            {
+                DataReader = new StreamReader(DataStream);
+                Payload = DataReader.ReadToEnd();
+            }
+            HttpResponse.Close();
+
+            Console.WriteLine(Payload);
+
+        }
+
+        public void fnPostMethod()
+        {
+
+        }
+
+        public void fnWriteTheResponse()
+        {
+
+        }
+
+
+    }
+}
